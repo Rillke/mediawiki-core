faf7e7dbb2a08a24441116f80aaeda645797155c
rillke <rainerrillke@hotmail.com>
2014-06-02 15:05:11 +0200

    Credits

--- extensions/PagedTiffHandler/PagedTiffHandler.php
+++ extensions/PagedTiffHandler/PagedTiffHandler.php
@@ -1,24 +1,27 @@
 <?php
 /**
- * Copyright © Wikimedia Deutschland, 2009
- * Authors Hallo Welt! Medienwerkstatt GmbH
- * Authors Sebastian Ulbricht, Daniel Lynge, Marc Reymann, Markus Glaser
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, write to the Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
- * http://www.gnu.org/copyleft/gpl.html
+ * Derivative work of:
  */
+	/**
+	* Copyright © Wikimedia Deutschland, 2009
+	* Authors Hallo Welt! Medienwerkstatt GmbH
+	* Authors Sebastian Ulbricht, Daniel Lynge, Marc Reymann, Markus Glaser
+	*
+	* This program is free software; you can redistribute it and/or modify
+	* it under the terms of the GNU General Public License as published by
+	* the Free Software Foundation; either version 2 of the License, or
+	* (at your option) any later version.
+	*
+	* This program is distributed in the hope that it will be useful,
+	* but WITHOUT ANY WARRANTY; without even the implied warranty of
+	* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+	* GNU General Public License for more details.
+	*
+	* You should have received a copy of the GNU General Public License along
+	* with this program; if not, write to the Free Software Foundation, Inc.,
+	* 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+	* http://www.gnu.org/copyleft/gpl.html
+	*/
 
 # Not a valid entry point, skip unless MEDIAWIKI is defined
 if ( !defined( 'MEDIAWIKI' ) ) {
